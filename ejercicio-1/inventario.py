86e0385cebf5f889b96ac061fca70591b4d46de3
Natalia Lazareva <natalilazareva01@gmail.com>
2025-02-12 20:25:12 +0100

    interfaz inicial creada

---
+++ ejercicio-1/inventario.py
@@ -0,0 +1,43 @@
+def mostrar_menu():
+    print("\n## Bienvenido al Sistema de Inventario")
+    print("\n1. Agregar productos al inventario")
+    print("2. Eliminar producto del inventario")
+    print("3. Mostrar inventario")
+    print("4. Consultar producto")
+    print("5. Salir")
+
+def mostrar_inventario(inventario):
+    if not inventario:
+        print("Por favor, introduce un producto")
+    for product in inventario:
+        print(f"Nombre del producto: {product['nombre']} | "
+              f"Precio: {product['precio']} | "
+              f"Cantidad: {product['cantidad']}")
+
+if __name__ == "__main__":
+    inventario = []
+
+    while True:
+        mostrar_menu()
+        opcion = input("Elige una opcion: ")
+
+        if opcion == "1":
+            nombre = input("Introduce el nombre del producto: ")
+            precio = float(input("Introduce el precio del producto: "))
+            cantidad = int(input("Introduce la cantidad de la producto: "))
+
+            producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
+            inventario.append(producto)
+
+            mostrar_inventario(inventario)
+        # elif opcion == "2":
+        #     #todo
+        elif opcion == "3":
+            mostrar_inventario(inventario)
+        # elif opcion == "4":
+        #     #todo
+        elif opcion == "5":
+            print("Finalizando la ejecución del programa...")
+            break
+        else:
+            print("Opción no válida")
